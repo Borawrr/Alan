fb21037db64193617ee974c21b5ab0fdf6661bf6
Bora Tekin <pikacu1903@gmail.com>
2023-12-09 13:01:05 +0300

    Create Alan.java

---
+++ Alan.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Alan {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner (System.in);
+
+        int kenar1;
+        int kenar2;
+        int kenar3;
+
+        System.out.println("1.Kenari Giriniz : ");
+        kenar1 = scanner.nextInt();
+        System.out.println("2.Kenari Giriniz : ");
+        kenar2 = scanner.nextInt();
+        System.out.println("3.Kenari Giriniz : ");
+        kenar3 = scanner.nextInt();
+
+        double u = ((kenar1 + kenar2 + kenar3) / 2);
+
+        double Alan = (u * (u-kenar1)* (u-kenar2)* (u-kenar3));
+
+        System.out.println("Ucgeninizin Alani : " + Math.sqrt(Alan));
+
+    }
+
+}
